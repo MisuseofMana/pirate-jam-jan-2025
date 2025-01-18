2c3583dd92d8b5b40e2d84e958dded86c28885fe
Sean Yager <contact@seanyager.com>
2025-01-18 15:39:16 -0500

    add meeple stats, code health, and rename files

--- components/meeple/meeple.gd
+++ components/meeple/meeple.gd
@@ -1,1 +1,8 @@
 class_name Meeple extends Node2D
+
+@onready var hearts: Node2D = $Hearts
+@export var stats : MeepleStats
+
+func _ready():
+	for heart : AnimatedSprite2D in hearts.get_children():
+		pass
