031d9376f1c2a3991ca02978e05506808a79d5a1
Sean Yager <contact@seanyager.com>
2025-01-18 17:11:59 -0500

    add trap scene and script for trap

---
+++ components/trap/trap.gd
@@ -0,0 +1,16 @@
+extends AnimatedSprite2D
+class_name Trap
+
+@onready var timer: Timer = $Timer
+@export_range(0.25, 10, 0.25) var trap_cooldown : float = 0.25
+
+func _ready():
+	timer.wait_time = trap_cooldown
+
+func trigger_trap():
+	if frame != 1:
+		frame = 1
+		timer.start()
+	
+func reset_trap():
+	frame = 0
