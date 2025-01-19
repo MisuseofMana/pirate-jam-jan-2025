cc25d7fa88c3afad92dc95b88993d173ef1bee28
Sean Yager <contact@seanyager.com>
2025-01-19 15:04:56 -0500

    update thought assets

--- scenes/secret_scene.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-@onready var gotem = $AudioStreamPlayer2D
-
-func _ready():
-	gotem.play()
-
-func _on_button_pressed():
-	SceneSwitcher.switch_scene("res://scenes/start_screen.tscn")
--- scenes/start_screen.gd
+++
@@ -1,7 +0,0 @@
-extends CanvasLayer
-
-func _on_button_pressed():
-	SceneSwitcher.switch_scene("res://scenes/logo_screen.tscn")
-
-func _on_secret_button_pressed():
-	SceneSwitcher.switch_scene("res://scenes/secret_scene.tscn")
