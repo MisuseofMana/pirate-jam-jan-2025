a2b65479d338990d0921c42beee7b52e46f86827
Sean Yager <contact@seanyager.com>
2025-01-19 23:40:16 -0500

    fix missing resource in meeple.tscn

--- utilities/ErrorUtil.gd
+++ utilities/ErrorUtil.gd
@@ -2,4 +2,4 @@ class_name ErrorUtil
 
 ## Return the result of this method to indicate that a method must be overridden in a subclass.
 static func assert_abstract():
-    assert(false, "This method must be overridden in a subclass.")
+	assert(false, "This method must be overridden in a subclass.")
