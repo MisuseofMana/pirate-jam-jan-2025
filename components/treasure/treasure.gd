3371cf14d8db159a40d806af82ef2c31dcac06fb
Sean Yager <contact@seanyager.com>
2025-01-20 12:24:08 -0500

    add treasure art, modify soul colors, fix broken room resources

--- components/treasure/treasure.gd
+++ components/treasure/treasure.gd
@@ -1,4 +1,16 @@
 class_name Treasure extends Node2D
 
+@onready var sprite = $Sprite2D
+
+const spriteOptions : Array[CompressedTexture2D] = [
+	preload("res://art/treasure/bar.png"),
+	preload("res://art/treasure/coins.png"),
+	preload("res://art/treasure/cup.png"),
+	preload("res://art/treasure/egg.png"),
+	preload("res://art/treasure/necklace.png"),
+	preload("res://art/treasure/sigil.png")
+]
+
 func _ready() -> void:
-    add_to_group("macguffin")
+	add_to_group("macguffin")
+	sprite.texture = spriteOptions.pick_random()
